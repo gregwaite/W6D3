7531c7ec423cf3a5e64448bf06c9aac8b49ecd0c
G <waite68@gmail.com>
2019-01-09 17:57:26 -0500

    Add handlers to tweet_compose

--- skeleton/frontend/tweet_compose.js
+++ skeleton/frontend/tweet_compose.js
@@ -16,5 +16,25 @@ class TweetCompose {
 
     return $(html);
   }
+
+  clearInput() {
+    this.$input.val('');
+    this.$mentionedUsersDiv.find('ul').empty();
+    this.$el.find(':input').prop('disabled', false);
+    this.$el.find('.char-left').empty();
+  }
+
+  handleInput(event) {
+    const inputLength = this.$input.val().length;
+
+    this.$el.find('.char-left').text(`${140 - inputLength} characters left`);
+  }
+
+  handleSuccess(data) {
+    const $tweetsUl = $(this.$el.data('tweets-ul'));
+    $tweetsUl.trigger('insert-tweet', data);
+
+    this.clearInput();
+  }
 }
 module.exports = TweetCompose;
