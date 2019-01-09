11a6792f2b2e2dcac24a082133e15d82cfb5e00a
G <waite68@gmail.com>
2019-01-09 12:35:37 -0500

    Make API Util and searchUsers

---
+++ skeleton/frontend/api_util.js
@@ -0,0 +1,13 @@
+const APIUtil = {
+
+  searchUsers: query => (
+    $.ajax({
+      url: '/users/search',
+      dataType: 'json',
+      method: 'GET',
+      data: { query }
+    })
+  ),
+}
+
+module.exports = APIUtil;
