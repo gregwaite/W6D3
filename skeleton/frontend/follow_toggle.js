9b6c92944359a0de130feb9a6ee5b4d6e5a9c47e
G <waite68@gmail.com>
2019-01-09 11:19:35 -0500

    Start followtoggle

---
+++ skeleton/frontend/follow_toggle.js
@@ -0,0 +1,39 @@
+const APIUtil = require('./api_util');
+
+class FollowToggle {
+  constructor(el, options) {
+    this.$el = $(el);
+    this.userId = this.$el.data('user-id') || options.userId;
+    this.followState = (this.$el.data('initial-follow-state') ||
+      options.followState);
+    this.render();
+
+    this.$el.on('click', this.handleClick.bind(this));
+  }
+
+  handleClick(event) {
+    const followToggle = this;
+
+    event.preventDefault();
+
+    if (this.followState === 'followed') {
+      this.followState = 'unfollowing';
+      this.render();
+      APIUtil.unfollowUser(this.userId).then(() => {
+        followToggle.followState = 'unfollowed';
+        followToggle.render();
+      });
+    } else if (this.followState === 'unfollowed') {
+      this.followState = 'following';
+      this.render();
+      APIUtil.followUser(this.userId).then(() => {
+        followToggle.followState = 'followed';
+        followToggle.render();
+      });
+    }
+  }
+
+  
+}
+
+module.exports = FollowToggle;
